208fce3cdf53f401fe2c1b35f16b4d5d814ccd68
david <davidfranciscotoro@gmail.com>
2021-08-31 12:07:07 -0500

    Entrar

--- tienda/productos/forms.py
+++ tienda/productos/forms.py
@@ -3,6 +3,6 @@ from django.forms import fields
 
 class ContactForm(forms.Form): #formulario cont todos los datos respectivos
 	nombre= forms.CharField(max_length=500)
-	numero_celular= forms.CharField(max_length=10)
+	numero_celular= forms.IntegerField()
 	email = forms.EmailField(max_length = 150)
-	mensage= forms.CharField(widget = forms.Textarea, max_length = 2000)
+	mensage= forms.CharField(required=False,widget = forms.Textarea, max_length = 2000,)
